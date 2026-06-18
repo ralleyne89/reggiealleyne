987e5e54f1dc362c4b9cbb06496d2c74566a9265
Reggie Alleyne <reggiealleyne@Reggies-MacBook-Pro.local>
2026-06-18 13:59:42 -0700

    Remove homepage proof band

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,6 @@ import HomeLoader from "@/components/home/HomeLoader";
 import HeroSection from "@/components/home/HeroSection";
 import HomeMarquee from "@/components/home/HomeMarquee";
 import FeaturedProjectsSection from "@/components/home/FeaturedProjectsSection";
-import ProofBand from "@/components/home/ProofBand";
 import PlaygroundPreviewSection from "@/components/home/PlaygroundPreviewSection";
 import ToolStackSection from "@/components/home/ToolStackSection";
 import AboutSection from "@/components/home/AboutSection";
@@ -16,7 +15,6 @@ const Index = () => {
       <HeroSection />
       <HomeMarquee />
       <FeaturedProjectsSection />
-      <ProofBand />
       <PlaygroundPreviewSection />
       <ToolStackSection />
       <AboutSection />
